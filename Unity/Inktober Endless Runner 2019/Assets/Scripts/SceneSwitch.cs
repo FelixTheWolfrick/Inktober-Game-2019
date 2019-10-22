11cfa75c74eae31afe57df8b2eb215e32c021f1f
FelixTheWolfrick <merlin3000productions@gmail.com>
2019-10-22 03:40:22 -0400

    Added updated arwork and title screen

---
+++ Unity/Inktober Endless Runner 2019/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Goes to title screen
+    public void GotoTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    //Go to credits
+    public void GotoCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    //Go to game
+    public void GotoGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
